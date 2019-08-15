bdc8b04530f43239e8528a0580130a171b86de45
Pim Stolk <pim@bankingofthings.io>
2019-08-15 15:26:30 +0200

    Change dependency to own fork
    Signed-off-by: Pim Stolk <pim@bankingofthings.io>

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         //.package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.1.0"),
-        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("master")),
+        .package(url: "https://github.com/BankingofThings/Swift-Kuery.git", .branch("group_concat_support")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
